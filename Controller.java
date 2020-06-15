cd5fc09af4cbba4861d48973af6a3fbffbc78cf4
erisera19 <58450146+erisera19@users.noreply.github.com>
2020-06-16 01:42:28 +0200

    Controller per admin

---
+++ Controller.java
@@ -0,0 +1,57 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+    @FXML
+    private Button btnProfesor;
+
+    @FXML
+    private Button btnLendet;
+
+    @FXML
+    private Button btnStudentet;
+
+    @FXML
+    private Pane pnStatus;
+
+    @FXML
+    private Label lbStatus;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+
+    }
+
+    @FXML
+
+
+    public void handleClicks(javafx.event.ActionEvent event) {
+        if(event.getSource()==btnStudentet)
+        {
+            lbStatus.setText("Studentet");
+
+        }
+        else if(event.getSource()==btnProfesor){
+            lbStatus.setText("Profesoret");
+
+        }
+
+        else if(event.getSource()==btnLendet){
+            lbStatus.setText("Lendet");
+
+        }
+    }
+
+}
+
+
